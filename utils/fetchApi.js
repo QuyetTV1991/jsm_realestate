dcb70846769989ccb85311abf210ef4a13c680a4
unknown <quyettv.bk@gmail.com>
2021-12-01 22:19:38 +0700

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '07c4382e13msh4ac030eef026115p108557jsnfa843319b92c'
+        }
+    })
+
+    return data
+}
